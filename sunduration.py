5757352970d0d8e252ca9fafb14b476ea49e82d8
hoetzgit <41108116+hoetzgit@users.noreply.github.com>
2022-11-23 15:40:16 +0100

    ups

--- sunduration.py
+++ sunduration.py
@@ -3,9 +3,6 @@ sunduration.py
 
 based on code from https://github.com/Jterrettaz/sunduration
 
-Determination of some values to better compare current weather
-conditions delivered via an API with local conditions.
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
